60943e80a7450a1c10519e1c51ca1d02d5037cf9
alaminindex <socialindex.js@gmail.com>
2024-04-16 17:36:11 +0600

    done!

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,57 @@
+function handleKeyupEvent(e){
+    const playerPressed = e.key;
+    if(playerPressed === 'Escape'){
+        gameOver()
+    }
+    const currentAlphabetElement = document.getElementById('currentAlphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLocaleLowerCase()
+
+    if (playerPressed === expectedAlphabet) {
+
+        // const score = document.getElementById('score')
+        // const currentScoreText = score.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // // console.log(currentScore);
+        // const newScore = currentScore + 1;
+        // // console.log(newScore);
+        // score.innerText = newScore
+        // --------------------------------------------
+        const score = geTextElementValueById('score');
+        const newScore = score + 1;
+        setTextElementValueById('score', newScore)
+        // console.log(newScore);
+
+
+        removeBackgroundColorPerKey(expectedAlphabet)
+        continueGame()
+        
+    } else {
+        // const life = document.getElementById('life')
+        // const currentLifeText = life.innerText;
+        // const currentLife = parseInt(currentLifeText);
+        // const newLife = currentLife - 1;
+        // life.innerText = newLife
+        // --------------------------------
+
+        const life = geTextElementValueById('life');
+        const newLife = life - 1;
+        setTextElementValueById('life', newLife)
+        if (newLife === 0) {
+            gameOver()
+        } else {
+            
+        }
+    }
+
+    
+
+
+}
+
+document.addEventListener('keyup', handleKeyupEvent
+)
+
 function continueGame() {
     const alphabet = getRandomAlphabet()
     // console.log('My Alphabet',alphabet);
@@ -7,5 +61,25 @@ function continueGame() {
 }
 
 function play() {
+    hideElementById('playgroundScreen')
+    showElementById('finalScore')
+    showElementById('homeScreen')
+
+    setTextElementValueById('life',5)
+    setTextElementValueById('score',0)
+
     continueGame()
+}
+
+function gameOver() {
+    showElementById('playgroundScreen')
+    hideElementById('finalScore');
+
+    const lastScore = geTextElementValueById('score');
+    // console.log(lastScore);
+    setTextElementValueById('finalScoreText',lastScore);
+
+    const alphabet = geTextElementById('currentAlphabet')
+    removeBackgroundColorPerKey(alphabet)
+
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,13 +1,45 @@
-document.getElementById('playNowBtn').addEventListener('click', function(){
-    const playgroundScreen = document.getElementById('playgroundScreen');
+// document.getElementById('playNowBtn').addEventListener('click', function(){
+//     const playgroundScreen = document.getElementById('playgroundScreen');
+//    playgroundScreen.classList.remove('hidden')
+//     const homeScreen = document.getElementById('homeScreen');
+//     homeScreen.classList.add('hidden')
+// })
+
+function hideElementById(elementId) {
+    const playgroundScreen = document.getElementById(elementId);
    playgroundScreen.classList.remove('hidden')
-    const homeScreen = document.getElementById('homeScreen');
+}
+function showElementById(elementId) {
+    const homeScreen = document.getElementById(elementId);
     homeScreen.classList.add('hidden')
-})
+}
+
+function geTextElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText)
+    return value;
+}
+
+function setTextElementValueById(elementId, value) {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function geTextElementById(elementId) {
+    const element = document.getElementById(elementId)
+    const text = element.innerText;
+    return text;
+}
+
 
 function setBackgroundColorPerKey(elementId) {
     const element = document.getElementById(elementId);
     element.classList.add('keyColor')
+}
+function removeBackgroundColorPerKey(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('keyColor')
 }
 
 function getRandomAlphabet(s) {
